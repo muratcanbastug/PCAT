bc6aff25e89debc80847bcbd7ff58c5b6b34c8e6
muratcanbastug <muratcanbastug1@gmail.com>
2022-08-03 21:04:45 +0300

    ejs

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 app = express();
 
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
+
+// use ve get middleware fonksiyonlarıdır.
+app.use(express.static('public'));
+
+const myLogger = (req, res, next) => {
+  console.log('Middleware Log 1');
+  next();
+};
+
+app.use(myLogger);
+
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('add');
 });
 
 app.listen(PORT, () => {
